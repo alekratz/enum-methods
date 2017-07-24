48efb69a3c527cd37444e91eb319c3f061cd9227
Alek Ratzloff <alekratz@gmail.com>
2017-07-24 15:28:24 -0400

    Add to_* getters and EnumToGetters derive, and tests for them.
    This is a relatively small commit that was a basic copy-paste and some
    change-ups.

--- src/lib.rs
+++ src/lib.rs
@@ -137,6 +137,14 @@ pub fn enum_into_getters(input: TokenStream) -> TokenStream {
     getters.parse().unwrap()
 }
 
+#[proc_macro_derive(EnumToGetters)]
+pub fn enum_to_getters(input: TokenStream) -> TokenStream {
+    let s = input.to_string();
+    let ast = parse_derive_input(&s).unwrap();
+    let getters = impl_enum_to_getters(&ast);
+    getters.parse().unwrap()
+}
+
 #[proc_macro_derive(EnumIsA)]
 pub fn enum_is_a(input: TokenStream) -> TokenStream {
     let s = input.to_string();
@@ -261,6 +269,57 @@ fn impl_enum_into_getters(ast: &DeriveInput) -> quote::Tokens {
         }
     }
 }
+
+fn impl_enum_to_getters(ast: &DeriveInput) -> quote::Tokens {
+    let ref name = ast.ident;
+
+    let variants =
+        if let Body::Enum(ref e) = ast.body { e }
+        else { unreachable!() };
+
+    macro_rules! getter_filter {
+        () => {
+            variants.iter()
+                .filter(|v| if let VariantData::Tuple(_) = v.data { true } else { false })
+                .filter(|v| v.data.fields().len() == 1)
+        };
+    }
+
+
+    let variant_names = getter_filter!()
+        .map(|v| v.ident.clone())
+        .collect::<Vec<Ident>>();
+
+    let function_names = getter_filter!()
+        .map(|v| format!("to_{}", to_snake_case(&v.ident)).into())
+        .collect::<Vec<Ident>>();
+
+    let function_name_strs = getter_filter!()
+        .map(|v| v.ident.to_string().to_lowercase())
+        .collect::<Vec<String>>();
+
+    let variant_types = getter_filter!()
+        .map(|v| v.data.fields()[0].ty.clone())
+        .collect::<Vec<Ty>>();
+
+    let getter_names = vec!(name.clone(); variant_types.len());
+
+    quote! {
+        #[allow(dead_code)]
+        impl #name {
+            #(pub fn #function_names(&self) -> #variant_types {
+                    if let &#getter_names::#variant_names(ref v) = self {
+                        v.clone()
+                    }
+                    else {
+                        panic!(concat!("called ", #function_name_strs, "() on {:?}"), self);
+                    }
+                }
+            )*
+        }
+    }
+}
+
 
 fn impl_enum_is_a(ast: &DeriveInput) -> quote::Tokens {
     let ref name = ast.ident;
