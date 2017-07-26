4ff7b82addb75e52625ac735ba9a7722c9b7225c
Daniel Kolsoi <thadan64@gmail.com>
2017-07-25 22:36:32 -0400

    Initial as_* test

--- src/getters.rs
+++ src/getters.rs
@@ -13,35 +13,38 @@ pub(crate) fn impl_enum_as_getters(ast: &DeriveInput) -> quote::Tokens {
         () => {
             variants.iter()
                 .filter(|v| if let VariantData::Tuple(_) = v.data { true } else { false })
-                .filter(|v| v.data.fields().len() == 1)
         };
     }
 
 
     let variant_names = getter_filter!()
+        .filter(|v| v.data.fields().len() == 1)
         .map(|v| v.ident.clone())
         .collect::<Vec<Ident>>();
 
     let function_names = getter_filter!()
+        .filter(|v| v.data.fields().len() == 1)
         .map(|v| format!("as_{}", to_snake_case(&v.ident)).into())
         .collect::<Vec<Ident>>();
 
     let function_name_strs = getter_filter!()
+        .filter(|v| v.data.fields().len() == 1)
         .map(|v| v.ident.to_string().to_lowercase())
         .collect::<Vec<String>>();
 
-    let variant_types = getter_filter!()
+    let variant_types_single = getter_filter!()
+        .filter(|v| v.data.fields().len() == 1)
         .map(|v| &v.data.fields()[0].ty)
         .map(|ty| Ty::Rptr(None, Box::new(MutTy { ty: ty.clone(), mutability: Mutability::Immutable })))
         .collect::<Vec<Ty>>();
 
-    let getter_names = vec!(name.clone(); variant_types.len());
+    let getter_names_single = vec!(name.clone(); variant_types_single.len());
 
-    quote! {
+    let mut tokens = quote! {
         #[allow(dead_code)]
         impl #name {
-            #(pub fn #function_names(&self) -> #variant_types {
-                    if let &#getter_names::#variant_names(ref v) = self {
+            #(pub fn #function_names(&self) -> #variant_types_single {
+                    if let &#getter_names_single::#variant_names(ref v) = self {
                         v
                     }
                     else {
@@ -50,7 +53,46 @@ pub(crate) fn impl_enum_as_getters(ast: &DeriveInput) -> quote::Tokens {
                 }
             )*
         }
-    }
+    };
+
+    let variant_names = getter_filter!()
+        .filter(|v| v.data.fields().len() > 1)
+        .map(|v| v.ident.clone())
+        .collect::<Vec<Ident>>();
+
+    let function_names = getter_filter!()
+        .filter(|v| v.data.fields().len() > 1)
+        .map(|v| format!("as_{}", to_snake_case(&v.ident)).into())
+        .collect::<Vec<Ident>>();
+
+    let function_name_strs = getter_filter!()
+        .filter(|v| v.data.fields().len() > 1)
+        .map(|v| v.ident.to_string().to_lowercase())
+        .collect::<Vec<String>>();
+
+    let variant_types_multiple = getter_filter!()
+        .filter(|v| v.data.fields().len() > 1)
+        .map(|v| Ty::Tup(v.data.fields().iter().map(|field| Ty::Rptr(None, Box::new(MutTy { ty: field.ty.clone(), mutability: Mutability::Immutable }))).collect::<Vec<Ty>>()))
+        .collect::<Vec<Ty>>();
+
+    let getter_names_multiple = vec!(name.clone(); variant_types_multiple.len());
+
+    tokens.append(quote! {
+        #[allow(dead_code)]
+        impl #name {
+            #(pub fn #function_names(&self) -> #variant_types_multiple {
+                    if let &#getter_names_multiple::#variant_names(ref v) = self {
+                        v
+                    }
+                    else {
+                        panic!(concat!("called ", #function_name_strs, "() on {:?}"), self);
+                    }
+                }
+            )*
+        }
+    });
+
+    tokens
 }
 
 pub(crate) fn impl_enum_into_getters(ast: &DeriveInput) -> quote::Tokens {
